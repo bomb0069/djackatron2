0014bfa1e6083b02193f0632520ef0c908ea938c
Karan Sivarat <bomb0069@gmail.com>
2013-07-13 12:50:49 +0700

    refactor test to parameterised

--- src/test/java/com/test/djackatron2/service/TestFlatFeePolicy.java
+++ src/test/java/com/test/djackatron2/service/TestFlatFeePolicy.java
@@ -1,31 +1,52 @@
 package com.test.djackatron2.service;
 
-import static org.junit.Assert.assertEquals;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+import java.util.Arrays;
+import java.util.Collection;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
+@RunWith(value=Parameterized.class)
 public class TestFlatFeePolicy {
+	
+	double fixRate;
+	double amount;
+	double transferFee;
+	
+	public TestFlatFeePolicy(double fixRate,	double amount, double transferFee ) {
+		this.fixRate = fixRate;
+		this.amount = amount;
+		this.transferFee = transferFee;
+	}
+	
+	@Parameters
+	public static Collection<Object[]> primeNumbers() {
+		return Arrays.asList(new Object[][] {
+				{ 5, 1, 5},
+				{ 5, 10, 5},
+				{ 5, 100, 5},
+				{ 5, 1000, 5},
+				{ 5, 10000, 5},
+				{ 3, 1, 3},
+				{ 3, 10, 3},
+				{ 3, 100, 3},
+				{ 3, 1000, 3},
+				{ 3, 10000, 3}
+		});
+		
+	}
 	
 	@Test
 	public void testFixRatingTo5AndItWillGetThatWhenWeCalculateFee () {
-		double fixedTranferRate = 5;
-		double transferAmount = 100;
-		FeePolicy feePolicy = new FlatFeePolicy(fixedTranferRate);
+		FeePolicy feePolicy = new FlatFeePolicy(fixRate);
 		
-		double tranferFee = feePolicy.calculateTranferRate(transferAmount);
+		double tranferFee = feePolicy.calculateTranferRate(amount);
 		
-		assertEquals(fixedTranferRate,tranferFee,0);
-
-		transferAmount = 10;
-		
-		tranferFee = feePolicy.calculateTranferRate(transferAmount);
-		
-		assertEquals(fixedTranferRate,tranferFee,0);
-
-		transferAmount = 1;
-		
-		tranferFee = feePolicy.calculateTranferRate(transferAmount);
-		
-		assertEquals(fixedTranferRate,tranferFee,0);
+		assertThat(tranferFee,equalTo(fixRate)) ;
 	}
 }
